c472ed58ab8bf8a086735447fa429aa0e9d49906
Matei Zaharia <matei.zaharia@gmail.com>
2018-05-12 18:47:28 -0700

    stuff

---
+++ test.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+
+# import time
+import sys
+
+from mlflow import log_metric, log_parameter, log_output_files
+
+if __name__ == "__main__":
+    print("Running {}".format(sys.argv[0]))
+    log_parameter("param1", 5)
+    # time.sleep(5)
+    log_metric("foo", 5)
+    # time.sleep(5)
+    log_metric("foo", 6)
+    # time.sleep(5)
+    log_metric("foo", 7)
+    log_output_files("outputs")
