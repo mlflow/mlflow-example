ea2dbed5efda8755b563659afe8b622632276339
Matei Zaharia <mateiz@users.noreply.github.com>
2018-06-02 11:29:46 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,13 @@ from __future__ import print_function
 # import time
 import sys
 
-from mlflow import log_metric, log_parameter, log_output_files
+from mlflow import log_metric, log_parameter, log_artifacts
 
 if __name__ == "__main__":
     print("Running {}".format(sys.argv[0]))
     log_parameter("num_dimensions", sys.argv[1])
     log_parameter("regularization", sys.argv[2])
-    # time.sleep(5)
     log_metric("foo", 5)
-    # time.sleep(5)
     log_metric("foo", 6)
-    # time.sleep(5)
     log_metric("foo", 7)
-    log_output_files("outputs")
+    log_artifacts("outputs")
