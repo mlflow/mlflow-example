16d60228d8eeefa96e66925ae00f4efafb9be91d
Andrew Chen <andrewmnchen@gmail.com>
2018-06-05 08:50:13 -0700

    init

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ import sys
 import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
 
@@ -42,29 +41,26 @@ if __name__ == "__main__":
     train_y = train[["quality"]]
     test_y = test[["quality"]]
 
-    alphas = [float(sys.argv[1])] if len(sys.argv) > 1 else [0, 0.5, 1]
-    l1_ratios = [float(sys.argv[2])] if len(sys.argv) > 2 else [0, 0.2, 0.5, 1]
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
-    # Try the following parameters for alpha.
-    for alpha in alphas:
-        for l1_ratio in l1_ratios:
-            with mlflow.start_run():
-                lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
-                lr.fit(train_x, train_y)
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
 
-                predicted_qualities = lr.predict(test_x)
+        predicted_qualities = lr.predict(test_x)
 
-                (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-                print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-                print("  RMSE: %s" % rmse)
-                print("  MAE: %s" % mae)
-                print("  R2: %s" % r2)
+        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
 
-                mlflow.log_parameter("alpha", alpha)
-                mlflow.log_parameter("l1_ratio", l1_ratio)
-                mlflow.log_metric("rmse", rmse)
-                mlflow.log_metric("r2", r2)
-                mlflow.log_metric("mae", mae)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
-                mlflow.sklearn.log_model(lr, "model")
+        mlflow.sklearn.log_model(lr, "model")
