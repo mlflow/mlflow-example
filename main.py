9cc5bb15f4a021cb1cd02e6405fbd30cc5d10124
cw-aniket <aniketwalse1@gmail.com>
2020-08-13 14:00:47 +0530

    fix

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-    mlflow.set_experiment("mlflow-projects")
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
